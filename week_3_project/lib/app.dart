54fe2d52cadca9ba3fcc5d826cf894c21ff308bc
fazal-e-haq <fazalehaq37405@GMAIL.COM>
2025-11-23 20:30:21 +0500

    add folders and files

---
+++ week_3_project/lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TaskManagementApp extends StatelessWidget {
+  const TaskManagementApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+
+
+    );
+  }
+}
